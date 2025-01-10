264279a34df0d62c9d8d5ffcd827f0dc7833d8e3
arpitbeuria196 <arpitbeuria196@gmail.com>
2025-01-10 11:32:37 +0000

    Beans

--- AppConfig.java
+++ AppConfig.java
@@ -1,2 +1,18 @@
-package com.codingShuttle.arpit.week1Introduction.introductionToSpringBoot;public class AppConfig {
+package com.codingShuttle.arpit.week1Introduction.introductionToSpringBoot;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    @Scope("prototype")
+    Apple getApple()
+    {
+        return new Apple();
+    }
+
 }
--- Apple.java
+++ Apple.java
@@ -1,2 +1,27 @@
-package com.codingShuttle.arpit.week1Introduction.introductionToSpringBoot;public class Apple {
+package com.codingShuttle.arpit.week1Introduction.introductionToSpringBoot;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+
+public class Apple {
+
+    void eatApple()
+    {
+        System.out.println("I am Eating Apple");
+    }
+
+   @PostConstruct// Before bean runs
+    void appEaten()
+    {
+        System.out.println("You Can't eat");
+    }
+
+    @PreDestroy // After destroy
+    void destroy()
+    {
+        System.out.println("Destroying the Apple Bean");
+    }
 }
